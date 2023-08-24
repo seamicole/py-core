d17aee26d8318c0697232d9a501182ebef40af1d
Sean O'Leary <seamicole@gmail.com>
2023-08-24 18:46:36 +0800

    Update collections __init__.py

---
+++ core/collections/__init__.py
@@ -0,0 +1,9 @@
+# ┌─────────────────────────────────────────────────────────────────────────────────────
+# │ PROJECT IMPORTS
+# └─────────────────────────────────────────────────────────────────────────────────────
+
+from core.collections.classes.item import Item as Item  # noqa: F401
+from core.collections.classes.store import Store as Store  # noqa: F401
+from core.collections.dict_collection import (  # noqa: F401
+    DictCollection as DictCollection,
+)
