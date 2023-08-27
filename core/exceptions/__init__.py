b78b3b4fcba452e788afb36f91bbeba3935fe075
Sean O'Leary <seamicole@gmail.com>
2023-08-27 13:42:31 +0800

    Implement NullAttributeError

--- core/exceptions/__init__.py
+++ core/exceptions/__init__.py
@@ -18,3 +18,25 @@ class Error(Exception):
 
         # Call super init
         super().__init__(message)
+
+
+# ┌─────────────────────────────────────────────────────────────────────────────────────
+# │ NULL ATTRIBUTE ERROR
+# └─────────────────────────────────────────────────────────────────────────────────────
+
+
+class NullAttributeError(Error):
+    """Raised when an expected non-null attribute is null"""
+
+    # ┌─────────────────────────────────────────────────────────────────────────────────
+    # │ __INIT__
+    # └─────────────────────────────────────────────────────────────────────────────────
+
+    def __init__(self, Class: type, attr: str) -> None:
+        """Init Method"""
+
+        # Set message
+        self.message = f"Attribute {Class.__name__}.{attr} should not be null."
+
+        # Call super init
+        super().__init__(self.message)
