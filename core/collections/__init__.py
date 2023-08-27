faf18fea8195c78dde946b8f66f43611bf2eaca0
Sean O'Leary <seamicole@gmail.com>
2023-08-27 15:58:54 +0800

    Remove Args and Kwargs types

--- core/collections/__init__.py
+++ core/collections/__init__.py
@@ -3,6 +3,9 @@
 # └─────────────────────────────────────────────────────────────────────────────────────
 
 from core.collections.classes.item import Item as Item  # noqa: F401
+from core.collections.classes.item_metaclass import (  # noqa: F401
+    ItemMetaclass as ItemMetaclass,
+)
 from core.collections.classes.store import Store as Store  # noqa: F401
 from core.collections.dict_collection import (  # noqa: F401
     DictCollection as DictCollection,
