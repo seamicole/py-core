f2f49a5ebddff4a04487515c600d6b768f6b2281
Sean O'Leary <seamicole@gmail.com>
2024-01-29 23:41:14 +0800

    Add dtnow function

--- core/datetime/functions/__init__.py
+++ core/datetime/functions/__init__.py
@@ -2,4 +2,6 @@
 # │ PROJECT IMPORTS
 # └─────────────────────────────────────────────────────────────────────────────────────
 
+from core.datetime.functions.dtnow import dtnow as dtnow  # noqa: F401
+from core.datetime.functions.dtnow import dtnow_utc as dtnow_utc  # noqa: F401
 from core.datetime.functions.dtto_utc import dtto_utc as dtto_utc  # noqa: F401
